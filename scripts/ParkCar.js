8828569dba63f1e3744bce413ef85527b25eda94
femi-oye <femoye@gmail.com>
2019-11-09 12:53:50 -0600

    Homework complete with Bode's assistance

--- scripts/ParkCar.js
+++ scripts/ParkCar.js
@@ -2,7 +2,7 @@
 const parkCarsDB = [];
 
 // create a fixed amount of spaces in the garage
-const garage = 2;
+const garage = 3;
 
 // create a class called Cars
 class Cars {
@@ -10,6 +10,14 @@ class Cars {
         this.carName = carName;
     } 
 }
+
+$ (document).ready(readyNow);
+
+function readyNow() {
+    console.log('JQ');
+    $ ('#submitBtn').on('click', getCarDetails);
+    $ ('#checkCarBtn').on('click', checkCarPark);
+} // end readyNow
 
 // function that grabs user input
 function getCarDetails(){
@@ -22,26 +30,18 @@ function getCarDetails(){
     }
 } // end getCarDetails
 
-$ (document).ready(readyNow);
-
-function readyNow() {
-    console.log('JQ');
-    $ ('#submitBtn').on('click', getCarDetails);
- 
-} // end readyNow
-
 // function to create new car and push to parkCars array for user input from getCarDetails
 function addToCarDetails(carName){
     if (parkCarsDB.length < garage){
         parkCarsDB.push(new Cars (carName));
-        console.log("Your " + carName + " is parked");
+       // console.log("Your " + carName + " is parked");
         displayCarDetails();
     } 
-        else
+        else{
         console.log("You cannot park here");
-    
-
-
+        $('#displayDiv').append(`<li> No Space to Park ${carName}</li>`);
+        }
+        
 }
 // function to display Cars to user
 function displayCarDetails(){
@@ -55,9 +55,43 @@ function displayCarDetails(){
 
 } // end displayCarDetails
 
-// function to display garage size
-function garageSpace(){
-    let space = parkCarsDB.length;
-    displaySpace();
-} // end garageSpace
 
+// function to display garage size
+// function garageSpace(){
+    // document.getElementById($('#checkSpaceDiv')).innerHTML = garage.length;
+//} // end garageSpace
+
+
+function garageSpace(){
+    let outputE2 = $('#displayDiv');
+    if (parkCarsDB.length === garage){
+        alert("There are no more spaces");   
+    }
+
+}
+
+// function to let user enter their car to check if it's in the garage after 
+function takeForSpin(car){
+    $('#carListDiv').empty();
+    for (kar of parkCarsDB){
+        if (kar.carName === car){
+            console.log("Your car is here");
+            $('#carListDiv').append(`<h2>Your car ${kar.carName} is parked here</h2>`);
+        }
+        else{
+            $('#carListDiv').append(`<h2>Your car was not parked here</h2>`);
+       }
+    }
+} // end checkForCar
+
+// function to get input for checking if car is the garage
+
+function checkCarPark(){
+    if ($('#carCheckIn').val() == ""){
+        alert("Fields cannot be empty");
+    } 
+    else{
+        takeForSpin($('#carCheckIn').val());
+        ($('#carCheckIn').val(""));
+    }
+} // end getCarDetails
